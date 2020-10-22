bfc7221ba4e43ec5bc46b773a530a360ab0c869b
Thais  dos S. Oliveira <joliveira@dcc.local>
2020-10-22 14:04:25 -0300

    inital commit

---
+++ CrudUsuarios/Models/EscolaridadeEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CrudUsuarios.Models
+{
+    public enum EscolaridadeEnum
+    {
+        INFANTIL = 1,
+        FUNDAMENTAL = 2,
+        MEDIO = 3,
+        SUPERIOR = 4
+    }
+}
